e96a3b4f9d69dd81519d15fc927a3c76d046d432
parth <parth.manocha2901@gmail.com>
2024-02-22 13:15:42 +0530

    Added changes!

--- static/script.js
+++ static/script.js
@@ -7,31 +7,7 @@ myButton.addEventListener('click', function() {
   window.open('https://github.com/', '_blank');
 });
 
-function clearErrors(){
-  errors = document.getElementsByClassName('formerror');
-
-  for (let item of errors){
-    item.innerHTML = "";
-
-  }
-
-}
-
-function seterror( id,error){
-  element = document.getElementById(id);
-  element.getElementByClassName('formerror')[0].innerHTML=error;
-}
-
-function validateform(){
-  var returnval = true;
-  clearErrors();
 
 
-  var name = document.forms['myForm']['Sname'].value;
-  if (name.length<5){
-    seterror("name", "*Length of name is too short");
-    returnval = false;
-}
-return returnval;
-}
+
 
